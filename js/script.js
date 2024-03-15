96169c25877eddba7181201206380d1e58d6d6f4
Frankosviz <geom.francescopaolo@gmail.com>
2024-03-15 15:18:32 +0100

    start

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function generateNewGridSquare(content){
+    const newElement = document.createElement('article');
+    newElement.innerHTML = '<span>' + content + '</span>';
+    newElement.classList.add('square');
+    return newElement;
+}
+console.log(generateNewGridSquare);
+
+const grid = document.getElementById('grid');
+console.log(grid);
+
+for (let index = 0; index < 100; index++){
+    const square = generateNewGridSquare(index + 1)
+    
+}
+
+
+
+// const square = addEventListener('click') function(){
---
+++ js/utility.js
@@ -0,0 +1,25 @@
+// funzione importante che utilizzeremo per farci tornare un numero random tra 1 e 100 
+
+//  function getRndInteger(min, max) {
+//      return Math.floor(Math.random() * (max - min + 1) ) + min;
+//    }
+
+
+// Date
+
+// const oggi = new Date ();
+// console.log(oggi);
+//  Ci da la data di oggi con l'ora ecc
+
+//  let currentYear = oggi.getFullYear();
+//  console.log(currentYear); 
+//  Corretto per estrarre anno corrente anche nei prossimi anni
+
+
+//   A variable or object which has a value is treated as a true boolean value. ‘0’, ‘NaN’, empty string, ‘undefined’, and ‘null’ is treated as false boolean values
+
+
+
+
+
+
