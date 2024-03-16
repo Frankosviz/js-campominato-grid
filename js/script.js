5f49693094ae5a316fe66c1f2ad5de8a256bc6ee
Frankosviz <geom.francescopaolo@gmail.com>
2024-03-16 11:32:11 +0100

    campominato end

--- js/script.js
+++ js/script.js
@@ -1,27 +1,50 @@
+// Creo la mia funzione senza contenuto
+
 function generateNewGridSquare(content){
+    
+// Aggiungo un elemento con classe square che ho creato in css
+
     const newElement = document.createElement('article');
     newElement.innerHTML = '<span>' + content + '</span>';
     newElement.classList.add('square');
     return newElement;
 }
-console.log(generateNewGridSquare);
+
+// console.log(generateNewGridSquare);
+
+// Seleziono la griglia assegnandogli le classi css che ho creato
 
 const grid = document.getElementById('grid');
+grid.classList.add('grid');
 console.log(grid);
+
+// Con il ciclo for vado ad aggiungere tante volte fino a 100 i miei quadrati con i numeri all'interno
 
 for (let index = 0; index < 100; index++){
     const square = generateNewGridSquare(index + 1);
-
-    console.log(square);
+    // console.log(square);
 
     square.addEventListener('click', function(){
-    square.classList.add('.bg-aqua');
-    console.log()
-    
+    square.classList.add('square', 'bg-aqua');
+    console.log(index + 1);
     });
-    gridElement.appendChild(square);
+
+
+
+// Con appendChild vado ad 'appendere' sulla mia pagina il contenuto di square
+
+    grid.appendChild(square);
 }
 
 
 
-// const square = addEventListener('click') function(){
+
+
+        // Ora creo la funzione che al click su play resetta i quadrati
+
+        // let play = document.getElementById('resetButton');
+        // play = addEventListener('click', function(){
+        //     square.classList.add('square', 'bg-white');
+        
+        // })
+
